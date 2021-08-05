5e08a10080d9faf4e104333fad3a3d789511b29c
guthaVamshi <79559118+guthaVamshi@users.noreply.github.com>
2021-08-05 16:23:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,11 +2,47 @@ var i = 0;
 var txt = 'vamshi gutha';
 var speed = 50;
 
-window.onload=function typeWriter() {
+window.onload = function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("vamshi").innerHTML +=txt.charAt(i);
+    document.getElementById("vamshi").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
 };
+function redirect() {
+  window.open('https://github.com/guthaVamshi/codeSmashers', '_blank');
+  modal.style.display = "none";
+}
+function redirect2() {
+  window.open('https://github.com/guthaVamshi/AMkart', '_blank');
+  modal2.style.display = "none";
+}
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+var nothanks = document.getElementById("nothanks");
+var nothanks2 = document.getElementById("nothanks2");
+var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+nothanks.onclick = function () {
+  modal.style.display = "none";
+}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+btn2.onclick = function () {
+  modal2.style.display = "block";
+}
+nothanks2.onclick = function () {
+  modal2.style.display = "none";
+}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal2.style.display = "none";
+  }
+}
 
